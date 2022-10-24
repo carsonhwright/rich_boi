97a3862bf150dc216d0c6c2fddb9b0aaebe5eb3a
Carson Wright <carson.wright@gtri.gatech.edu>
2022-10-24 14:01:13 -0400

    making progress using api

--- cpi_api.py
+++ cpi_api.py
@@ -1,10 +1,33 @@
 import requests
-import json
+import json, re
 import prettytable
+import pandas as pd
+def get_series_ids():
+    """"""
+    series_table_filepath = "output/series_id_table.txt"
+    headers = {'Content-type': 'application/json'}
+    # data = json.dumps({"seriesid", "startyear", "endyear", "begin_period", "end_period"})
+    out = requests.get('https://download.bls.gov/pub/time.series/cu/cu.series').content.decode('utf-8')
+    out = out.replace('\n\n', '\n')
+    out = out.replace('\r\n', '\n')
+    out = out.replace('\t' , '|')
+    out = re.sub('\s{2,}', '', out)
+    with open(series_table_filepath, 'w') as f:
+        f.write(out)
+    read_file = pd.read_csv(series_table_filepath, on_bad_lines='skip', delimiter='|')
+    read_file.to_csv("output/series_id_table.csv", index=None)
+    return read_file["series_id"]
+
 headers = {'Content-type': 'application/json'}
 data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"})
 p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
 json_data = json.loads(p.text)
+# THIS HAS NOT BEEN IMPLEMENTED YET, RIGHT NOW THIS ONLY PROVIDES THE IDS THAT THE API SHOULD BE
+# REQUESTING: the data variable will need to be changed or perhaps removed and the 'p' variable
+# perhaps should request all data and use the series ids to create new data sets individually
+# this will likely get hug
+gork = get_series_ids()
+breakpoint()
 for series in json_data['Results']['series']:
     x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
     seriesId = series['seriesID']
@@ -18,6 +41,6 @@ for series in json_data['Results']['series']:
                 footnotes = footnotes + footnote['text'] + ','
         if 'M01' <= period <= 'M12':
             x.add_row([seriesId,year,period,value,footnotes[0:-1]])
-    output = open(seriesId + '.txt','w')
+    output = open(f'output/{seriesId}' + '.txt','w')
     output.write (x.get_string())
     output.close()
