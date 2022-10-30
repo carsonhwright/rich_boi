fd54270fedfe144318820bc84f15b72785da31c7
carsonhwright <carsonhwright@gmail.com>
2022-10-30 03:08:12 -0400

    got the requests to work and make csvs rather than pretty tables need to add ability to request years in batches of 10 at a time for initial construction of database

--- cpi_api.py
+++ cpi_api.py
@@ -2,6 +2,7 @@ import requests
 import json, re, time
 import prettytable
 import pandas as pd
+import numpy as np
 
 REQUEST_LIMIT = 25
 DAILY_QUERY_LIMIT = 500
@@ -90,6 +91,47 @@ def get_daily_data():
         updated and when each individual series was pulled.
     """
 
+def pull_test_batch():
+    test_series = ['CUSR0000SA0']
+    # , 'CUSR0000SA0L12E4', 'CUSR0000SAC']
+    get_api_pd_batch_data(test_series)
+    # get_api_constrained_batch_data(test_series)
+
+def get_api_pd_batch_data(series_ids):
+    """
+    This will request batches of CPI series data, 25 (or whatever REQUEST_LIMIT) at a time. And
+    will wait 10s between requests. This doesn't work exactly as I would like, because there also
+    exists a 500 per day query limit. This will have to be accommodated for as well. See 
+    get_daily_data(). Also limits it to maximum time range is 10 years
+    """
+    headers = {'Content-type': 'application/json'}
+    series_general_df = pd.read_csv("output\\series_id_table.csv")
+    
+    for series_id in series_ids:
+
+        start_year = str(series_general_df.iloc[np.where(series_general_df["series_id"] == series_id)]["begin_year"][0] + 40)
+        # start_year = "2011"
+        # end_year = "2014"
+        series_id_list_obj = [series_id]
+        end_year = str(series_general_df.iloc[np.where(series_general_df["series_id"] == series_id)]["end_year"][0])
+        data = json.dumps({"seriesid": series_id_list_obj,"startyear": start_year, "endyear": end_year})
+
+        # now similarly as I did with the batches of requests, I need to batch the number of years requested
+        # in sets of 10
+
+        # ThIs FuCkInG tHiNg OnLy TaKeS lIsTs!!!1!!!!1!one111!11!!11! for the seriesid in the data dict
+        proc = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+
+        json_data = json.loads(proc.text)
+        try:
+            breakpoint()
+            data = json_data["data"]
+            for item in data: item["series_id"]=series_id
+            df = pd.DataFrame(data=data)
+            df.to_csv(f'output/{series_id}.csv', index=None)
+        except KeyError:
+            print(f"[KeyError] Failed to parse series file: {series_id}")
+            time.sleep(5)
 def main():
     series_ids = get_series_data_list()
     get_api_constrained_batch_data(series_ids)
